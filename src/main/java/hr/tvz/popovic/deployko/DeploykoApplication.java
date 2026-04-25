061439efeb767a43732bd00ad46c5661e5926f37
TihomirPop <tihomirpopovic42@gmail.com>
2026-04-25 18:11:59 +0200

    initial commit

---
+++ src/main/java/hr/tvz/popovic/deployko/DeploykoApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.popovic.deployko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploykoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploykoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tvz/popovic/deployko/DeploykoApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.popovic.deployko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploykoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
