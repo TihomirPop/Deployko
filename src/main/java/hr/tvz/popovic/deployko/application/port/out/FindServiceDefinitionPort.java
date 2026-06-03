2b8fedf449ac43553fff5ca5d79045e5c11d5abe
TihomirPop <tihomirpopovic42@gmail.com>
2026-06-03 13:54:47 +0200

    Add service definition lookup port

---
+++ src/main/java/hr/tvz/popovic/deployko/application/port/out/FindServiceDefinitionPort.java
@@ -0,0 +1,23 @@
+package hr.tvz.popovic.deployko.application.port.out;
+
+import hr.tvz.popovic.deployko.application.domain.model.ImageRepository;
+import hr.tvz.popovic.deployko.application.domain.model.ServiceName;
+
+public interface FindServiceDefinitionPort {
+
+    FindServiceDefinitionResult findByName(ServiceName serviceName);
+
+    sealed interface FindServiceDefinitionResult
+            permits FindServiceDefinitionResult.Found, FindServiceDefinitionResult.NotFound,
+            FindServiceDefinitionResult.Failure {
+
+        record Found(ServiceName serviceName, ImageRepository imageRepository) implements FindServiceDefinitionResult {
+        }
+
+        record NotFound() implements FindServiceDefinitionResult {
+        }
+
+        record Failure() implements FindServiceDefinitionResult {
+        }
+    }
+}
