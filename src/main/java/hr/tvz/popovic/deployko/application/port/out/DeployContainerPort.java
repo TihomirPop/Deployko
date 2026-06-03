1b822a14ea357c3b9ecb6695e4fbb902a9459d4f
TihomirPop <tihomirpopovic42@gmail.com>
2026-06-03 14:15:45 +0200

    Add deploy container port

---
+++ src/main/java/hr/tvz/popovic/deployko/application/port/out/DeployContainerPort.java
@@ -0,0 +1,18 @@
+package hr.tvz.popovic.deployko.application.port.out;
+
+import hr.tvz.popovic.deployko.application.domain.model.DesiredDeployment;
+
+public interface DeployContainerPort {
+
+    DeployContainerResult deploy(DesiredDeployment desiredDeployment);
+
+    sealed interface DeployContainerResult
+            permits DeployContainerResult.Success, DeployContainerResult.Failure {
+
+        record Success() implements DeployContainerResult {
+        }
+
+        record Failure() implements DeployContainerResult {
+        }
+    }
+}
